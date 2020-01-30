6ac93fce52fac4acc51787f76b85168655d5baa5
Michael Denman <denmanmd@gmail.com>
2020-01-30 15:24:05 -0600

    adds showHideButton component to resume

---
+++ src/components/ShowHideButton/ShowHideButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ShowHideButton(props) {
+    function showHide() {
+
+    }
+    return (
+        <button class="showhidebutton" onClick={props}>
+            Show/Hide Accomplishments
+        </button>
+    )
+}
