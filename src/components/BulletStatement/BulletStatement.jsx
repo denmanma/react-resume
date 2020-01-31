8c0d7949fcf4a8650c86c46a9d166b86a01387b3
Michael Denman <denmanmd@gmail.com>
2020-01-31 16:31:11 -0600

    adds expansion panel to BulletStatement. Still needs mobile adaptation.

--- src/components/BulletStatement/BulletStatement.jsx
+++ src/components/BulletStatement/BulletStatement.jsx
@@ -1,14 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-export default function BulletStatement({bullets}) {
-    const listBullets = bullets.map((bullet) =>
-      <li key={bullet}>
-        {bullet}
-      </li>
-    );
-    return (
-      <ul>{listBullets}</ul>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
   }
+}));
 
- 
+export default function BulletStatement({ bullets }) {
+  const classes = useStyles();
+  const listBullets = bullets.map(bullet => <li key={bullet}>{bullet}</li>);
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Show/Hide Bullets</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <ul>{listBullets}</ul>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
